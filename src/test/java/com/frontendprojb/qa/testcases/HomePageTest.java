c9bbe975f8f64ae9e268fb906630d15e8a4c206e
Sachin Roy <sachin.jr@gmail.com>
2023-06-26 10:11:17 -0400

    fixed

--- src/test/java/com/frontendprojb/qa/testcases/HomePageTest.java
+++ src/test/java/com/frontendprojb/qa/testcases/HomePageTest.java
@@ -43,6 +43,6 @@ public class HomePageTest extends BaseTest {
         storepage.searchProduct(product.getSearch_term());
         Assert.assertEquals(storepage.getProductName(), product.getName());
         Assert.assertEquals(storepage.getProductCategory(), product.getCategory());
-        Assert.assertEquals(storepage.getProductPrice(), "144");
+        Assert.assertEquals(storepage.getProductPrice(), product.getPrice());
     }
 }
